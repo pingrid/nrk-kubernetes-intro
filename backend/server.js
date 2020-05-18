09d502d138d8b7e994cbd5f7175fc35215af54db
Ingrid Grønlie Guren <ingrid.gronlie.guren@nrk.no>
2020-05-18 22:34:38 +0200

    Add backend code

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('winston');
+const app = express();
+const port = 5000;
+const { education, work, me } = require('./data');
+
+app.get('/', (request, response) => {
+	response.send(`Hello, I'm alive`);
+	logger.info('Responded to GET request on /')
+});
+
+app.get('/api/me', (request, response) => {
+	logger.info('Responded to GET request on /api/me')
+	response.send(JSON.stringify(me));
+});
+
+app.get('/api/work', (request, response) => {
+	logger.info('Responded to GET request on /api/work')
+	response.send(JSON.stringify(work));
+});
+
+app.get('/api/education', (request, response) => {
+	logger.info('Responded to GET request on /api/edu')
+	response.send(JSON.stringify(education));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return logger.error('something bad happened', err)
+  }
+
+  logger.info(`server is listening on ${port}`)
+});
